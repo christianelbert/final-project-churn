1cdec9f68460455c9d11a3c99950f80a929ce44c
christianelbert <christian_elbert96@yahoo.com>
2020-02-18 15:14:51 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# JCDS 07 Final Project
+
+![purwadhikaLogo](https://d1ah56qj523gwb.cloudfront.net/uploads/organizations/logos/1538557444-kcgv11HXelvcOnlyrGcEpfwAf6hbPMhC.png)
+
+# Telecom Customer Churning Classification
+
+#### By: Christian Elbert
+
+<hr>
+What is churning? Churning is the rate at which customers stop doing business with an entity
+
+Customer churning is a major issue for many businesses, telecom industry being no exception. This issue then will lead to company's financial loss and even reputation wise.
+
+There are many factors that contribute to churning in telecom industry, including customer service issue, product issue or competitors. Having the ability to predict churn will give company a better idea in forecasting future revenue.
+
+With the above in mind, I will attempt to make a machine learning model to identify this issue. The outcome desired is to determine factors affecting churn, make recommendation based on findings and predict churning outcome given user input. 
+
+The dataset used for this project is __["Telecom Churning"](https://www.kaggle.com/mnassrib/telecom-churn-datasets)__ provided by __["Kaggle"](https://www.kaggle.com/)__.
+
+In this web application, 4 html pages will be shown.
+
+1. Home
+    This is the first page shown when accessing web apps. In this page, you can choose between two options, Stats (view current statistics) and Predict (user will input data to make prediction).
+
+    ![Home](images/home.png)
+
+2. Stats
+    After user clicked Stats, user will be shown current statistics from the company. This includes current churning rate, average charges and subscription's top 5 state.
+
+    ![Stats](images/stats.png)
+
+3. Predict
+    This page will show which data can be used to make prediction. Data that will be used to make prediction includes monthly charges, customer service calls made, date join and US' state. 
+    
+    Data inputted then will be used to predict churning through machine learning model. 
+
+    ![Predict](images/predict.png)
+    ![Predict_1](images/predict1.png)
+
+4. Churn
+    This is the final page that will show prediction result. In this page, user can see the churning probabilty percentage, visualize in a graph and table comparing user's information to the average in a particular US' state.
+
+    ![Churn](images/churn.png)
+
+<hr>
+
+#### Christian Elbert _christian_elbert96@yahoo.com_
+[Github](https://github.com/christianelbert) | [LinkedIn](https://www.linkedin.com/in/christian-elbert-a45008b9/)
+
---
+++ app.py
@@ -0,0 +1,152 @@
+from flask import Flask, url_for, render_template, redirect, request, send_from_directory, abort, jsonify
+import joblib
+import plotly
+import plotly.graph_objs as go
+import chart_studio.plotly as csp
+import json
+import pandas as pd
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/input')
+def input():
+    return render_template('input.html')
+
+@app.route('/stats')
+def stats():
+    customer = {'total':len(df_full), 'churn':len(df_full[df_full['Churn']==1])}
+
+    pie_stats = go.Pie(labels=['Not Churn','Churn'],values=list(dict(full['Churn'].value_counts()).values()),hole=.6,textinfo='none')
+    stats_2 = json.dumps([pie_stats],cls=plotly.utils.PlotlyJSONEncoder)
+
+    impo = pd.Series(model.feature_importances_*100,df1.drop(columns=['Churn']).columns).sort_values(ascending=False).head(5)    
+    plot_stats = go.Bar(y=['DayCharge','CScalls','EveCharge','IntlCharge','IntlCall'][::-1],x=impo.values.tolist()[::-1],orientation='h')
+    stats_1 = json.dumps([plot_stats],cls=plotly.utils.PlotlyJSONEncoder)
+
+    state_top = dict(df_full['State'].value_counts().head())
+    state_stats = go.Bar(y=list(state_top.keys())[::-1],x=list(state_top.values())[::-1],orientation='h')
+    state_stats_1 = json.dumps([state_stats],cls=plotly.utils.PlotlyJSONEncoder)
+
+    state_churn_top = dict(df_full[df_full['Churn']==1]['State'].value_counts().head())
+    state_churn_stats = go.Bar(y=list(state_churn_top.keys())[::-1],x=list(state_churn_top.values())[::-1],orientation='h')
+    state_stats_2 = json.dumps([state_churn_stats],cls=plotly.utils.PlotlyJSONEncoder)
+
+    daycharge_0 = df_full[df_full['Churn']==0]['day_charge'].mean()
+    daycharge_1 = df_full[df_full['Churn']==1]['day_charge'].mean()
+    evecharge_0 = df_full[df_full['Churn']==0]['eve_charge'].mean()
+    evecharge_1 = df_full[df_full['Churn']==1]['eve_charge'].mean()
+    nightcharge_0 = df_full[df_full['Churn']==0]['night_charge'].mean()
+    nightcharge_1 = df_full[df_full['Churn']==1]['night_charge'].mean()
+    intcharge_0 = df_full[df_full['Churn']==0]['intl_charge'].mean()
+    intcharge_1 = df_full[df_full['Churn']==1]['intl_charge'].mean()
+    x = pd.DataFrame({
+        'Loyal': [daycharge_0,evecharge_0,nightcharge_0,intcharge_0],
+        'Churn': [daycharge_1,evecharge_1,nightcharge_1,intcharge_1]}, index = ['Day','Evening','Night','International'])
+    
+    intl_churn = go.Pie(labels=['Not Churn','Churn'],values=list(dict(df_full[df_full['Intl_plan']=='Yes']['Churn'].value_counts()).values()),hole=.6,textinfo='none')
+    intl_churn_pie = json.dumps([intl_churn],cls=plotly.utils.PlotlyJSONEncoder)
+
+    intl_nochurn = go.Pie(labels=['Not Churn','Churn'],values=list(dict(df_full[df_full['Intl_plan']=='No']['Churn'].value_counts()).values()),hole=.6,textinfo='none')
+    intl_nochurn_pie = json.dumps([intl_nochurn],cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template('stats.html',stats_1=stats_1,stats_2 = stats_2,state_stats_1=state_stats_1,customer = customer,state_stats_2=state_stats_2,tables=[x.to_html(classes='data')],pie3=intl_churn_pie,pie4=intl_nochurn_pie)
+
+@app.route('/churn',methods=['POST','GET'])
+def churn():
+    if request.method == 'POST':
+        body = request.form
+        state = body['state']
+        intl = body['intl_plan']
+        length = (pd.Timestamp(datetime.now()).to_period('M') - pd.Timestamp(body['length']).to_period('M')).n
+        combine = states[f'{state}']+intls[f'{intl}'] + [length,float(body['voicemail']),float(body['daycall']),float(body['daycharge']),float(body['evecall']),float(body['evecharge']),float(body['nightcall']),float(body['nightcharge']),float(body['intcall']),float(body['intcharge']),float(body['cscall'])]
+        predict = model.predict([combine])[0]
+
+        if predict == 0:
+            churn = 'Not Churn'
+        else:
+            churn =' Churn'
+
+        churn_proba = model.predict_proba([combine])[0]
+        proba_max = round(churn_proba.max()*100,1)
+
+        plot = go.Pie(labels=['Not Churn','Churn'],values=churn_proba,hole=0.6,textinfo='none')
+        graphJson = json.dumps([plot],cls=plotly.utils.PlotlyJSONEncoder)
+
+        dataframe = pd.DataFrame({
+            'User':[int(body['voicemail']),int(body['daycall']),float(body['daycharge']),int(body['evecall']),float(body['evecharge']),int(body['nightcall']),float(body['nightcharge']),int(body['intcall']),float(body['intcharge']),int(body['cscall'])],
+            'Average':[round(df_full[df_full['State']==state]['Voicemail_message'].mean(),2),round(df_full[df_full['State']==state]['day_call'].mean(),2),round(df_full[df_full['State']==state]['day_charge'].mean(),2),round(df_full[df_full['State']==state]['eve_call'].mean(),2),round(df_full[df_full['State']==state]['eve_charge'].mean(),2),round(df_full[df_full['State']==state]['night_call'].mean(),2),round(df_full[df_full['State']==state]['night_charge'].mean(),2),round(df_full[df_full['State']==state]['intl_call'].mean(),2),round(df_full[df_full['State']==state]['intl_charge'].mean(),2),round(df_full[df_full['State']==state]['CS_calls'].mean(),2)]
+            },index=['VoiceMail','Day Call','Day Charge','Evening Call','Evening Charge','Night Call','Night Charge','Intl Call','Intl Charge','CS Calls']
+        )
+        # print(dataframe)
+        return render_template('churn.html',proba = churn_proba, data = body,churnplot=graphJson,predict=churn,max=proba_max,data1=[dataframe.to_html(classes='data')])
+
+if __name__ == '__main__':
+    model = joblib.load('randomforest')
+    df_full = pd.read_csv('full.csv')
+    df1 = pd.read_csv('clean_train.csv')
+    full = pd.read_csv('clean_full.csv')
+    states = {
+        'AK':[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'AL':[0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'AR':[0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'AZ':[0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'CA':[0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'CO':[0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'CT':[0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'DC':[0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'DE':[0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'FL':[0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'GA':[0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'HI':[0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'IA':[0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'ID':[0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'IL':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'IN':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'KS':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'KY':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'LA':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'MA':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'MD':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'ME':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'MI':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'MN':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'MO':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'MS':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'MT':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'NC':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'ND':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'NE':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'NH':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'NJ':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'NM':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'NV':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'NY':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'OH':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'OK':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'OR':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        'PA':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0],
+        'RI':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+        'SC':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0],
+        'SD':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],
+        'TN':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+        'TX':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0],
+        'UT':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0],
+        'VA':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0],
+        'VT':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0],
+        'WA':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0],
+        'WI':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0],
+        'WV':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
+        'WY':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+    }
+    intls = {
+        'No': [1,0],
+        'Yes':[0,1]
+    }
+
+    app.run(debug=True)
